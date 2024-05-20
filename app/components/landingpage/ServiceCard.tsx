9341dbf6aca1c78e566a0f3fa3ce8167e895451e
Aayanjit <aayan.work99@gmail.com>
2024-05-20 21:55:45 +0530

    Added all images to public folder for emergency deployment. Will be removed back to assest folder after deployment.

--- app/components/landingpage/ServiceCard.tsx
+++ app/components/landingpage/ServiceCard.tsx
@@ -19,11 +19,11 @@ const ServiceCard: React.FC<ServiceCardProps> = ({ id, title, content, image, ro
     const [activeCard, setActiveCard] = useState<number | null>(null)
 
     return (
-        <div onMouseEnter={() => setActiveCard(id)} onMouseLeave={() => setActiveCard(null)} key={id} className='w-full relative overflow-hidden rounded-[20px] group'>
+        <div onMouseEnter={() => setActiveCard(id)} onMouseLeave={() => setActiveCard(null)} key={id} className='w-full relative overflow-hidden rounded-[20px] group my-2'>
             <div className='w-full h-full relative overflow-hidden'>
                 <Image src={image} width={300} height={400} className={`object-contain ${activeCard === id ? 'scale-[300%] blur-sm' : ''} transition-all duration-700 ease-in-out`} alt="" />
             </div>
-            <div className={`${activeCard === id ? 'pt-3 md:pt-8 xl:pt-10' : 'translate-y-[30%] md:translate-y-[18%]'} justify-end w-full px-6 pb-8 h-full flex flex-col items-center absolute left-0 bottom-0 overflow-hidden bg-gradient-to-b from-transparent to-black bg-opacity-60 transition-all duration-500 ease-in-out`}>
+            <div className={`${activeCard === id ? 'pt-3 md:pt-8 xl:pt-10' : 'translate-y-[16%] md:translate-y-[18%]'} justify-end w-full px-6 pb-8 h-full flex flex-col items-center absolute left-0 bottom-0 overflow-hidden bg-gradient-to-b from-transparent to-black bg-opacity-60 transition-all duration-500 ease-in-out`}>
                 <div className='flex flex-col gap-7'>
                     <h1 className={`text-xl md:text-[26px] leading-tight text-white font-[600] transition-all duration-500 ease-in-out text-wrap flex items-end ${activeCard === id ? '' : ''}`}>{title}</h1>
                     {/* <h1 className={`text-white text-sm md:text-xl font-[500] md:min-w-[200px] transition-all duration-500 ease-in-out ${activeCard === id ? 'opacity-100' : 'opacity-0'}`}>{content}</h1> */}
--- app/components/landingpage/ServicesLanding.tsx
+++ app/components/landingpage/ServicesLanding.tsx
@@ -22,7 +22,7 @@ function ServicesLanding() {
   return (
     <div className='pt-[50px] pb-[100px] px-10 md:px-[7rem]'>
       <TextHeading headingText='PRIMARY SERVICES' textPosition='center' />
-      <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:flex gap-x-2 xl:gap-0 gap-y-4 mt-24 justify-items-center'>
+      <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-y-4 mt-24 justify-center gap-4 justify-items-center'>
         {
           servicesList?.primaryServices?.map((data) => (
             <div className='w-fit pr-4' key={data?.id}>
